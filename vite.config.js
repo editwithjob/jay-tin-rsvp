69f95582e4e3742904201ea4b6e4f8c59e90f1f5
Jobaliermo <jobaliermo1738@gmail.com>
2026-04-27 23:55:46 +0800

    fix: update base for netlify

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/jay-tin-rsvp/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
